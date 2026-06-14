d63b0b884ba91a37b240fe187801df756995253d
ElmubakiWataara <elmubaki@gmail.com>
2026-06-14 14:08:21 +0000

    fix: added hover, checked if skip button is clicked and change button text

--- frontend/src/pages/VotingPage.jsx
+++ frontend/src/pages/VotingPage.jsx
@@ -83,12 +83,18 @@ const VotingPage = () => {
   };
 
   const skipPosition = (position) => {
-    setSkippedPositions((prev) => new Set(prev).add(position));
+    setSkippedPositions((prev) => {
+      const newSet = new Set(prev);
+      newSet.add(position);
+      return newSet;
+    });
+
     setSelections((prev) => {
       const newSelections = { ...prev };
       delete newSelections[position];
       return newSelections;
     });
+
     if (currentStep < positions.length - 1) {
       setCurrentStep(currentStep + 1);
     }
@@ -218,9 +224,15 @@ Please vote or skip all positions before submitting.`);
           </h2>
           <button
             onClick={() => skipPosition(currentPosition.position)}
-            className="px-6 py-2 mb-2 text-gray-600 border border-gray-300 hover:bg-gray-100 rounded-2xl transition text-lg font-medium "
+            className={`px-6 py-2 mb-2 border rounded-2xl transition text-lg font-medium ${
+              skippedPositions.has(currentPosition.position)
+                ? "bg-green-700 text-white border-green-700"
+                : "text-gray-600 border-gray-300 hover:bg-green-500 hover:text-white"
+            }`}
           >
-            Skip
+            {skippedPositions.has(currentPosition.position)
+              ? "Skipped"
+              : "Skip"}
           </button>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
